5080904d09647535a9021684fcea3b6a671540c3
arun <arunkumar39874@gmail.com>
2024-03-03 15:23:52 +0530

    commit

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-def predict_baldness(image_path, model):
-    img = image.load_img(image_path, target_size=(224, 224))
+def predict_baldness(img, model):
     img_array = image.img_to_array(img) / 255.0    # Convert to array and scale
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
 
@@ -20,20 +19,16 @@ def main():
     st.title("Baldness Detection")
 
     # Load the model
-    model_path = 'baldness_detection_model.h5'  # Adjust the path accordingly
-    model = load_model(model_path)
+    model = load_model('baldness_detection_model.h5')
 
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg"])
 
     if uploaded_file is not None:
-        image_path = "bald-image1.jpg"
-        with open(image_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-
-        st.image(image_path, caption="Uploaded Image", use_column_width=True)
+        img = image.load_img(uploaded_file, target_size=(224, 224))
+        st.image(img, caption="Uploaded Image", use_column_width=True)
         st.write("")
 
-        prediction = predict_baldness(image_path, model)
+        prediction = predict_baldness(img, model)
         st.write(f"Prediction: {prediction}")
 
 if __name__ == "__main__":
